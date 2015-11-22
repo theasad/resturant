c4413dbe0c2d023a22913f852a36512330f773fd
Md. Asaduzzaman <asaduzzaman.cse@gmail.com>
2015-11-23 02:03:20 +0600

    this is first commit

---
+++ html/js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+
+        // sticky menu
+        
+        $(".header-area").sticky({topSpacing:0});
+
+        //jQuery smooth scroll
+        $('li.smooth-menu a').bind('click', function(event) {
+        	var $anchor = $(this);
+        	var headerH = '68';
+        	$('html, body').stop().animate({
+        		scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
+        	}, 1200, 'easeInOutExpo');
+        	event.preventDefault();
+        });
+
+        $('body').scrollspy({ 
+            target: '.navbar-collapse' ,
+            offset: 76
+        });
+
+        $('.parallax-bg, .top-area-bg').scrolly({bgParallax: true});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
